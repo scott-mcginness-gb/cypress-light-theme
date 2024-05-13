dc6b69c7d916cb814ca5a765330d0eab64e7da82
Scott McGinness <scott.mcginness@glassboxdigital.com>
2024-05-13 14:59:28 +0100

    Copy the JSDoc onto the actual function

--- index.js
+++ index.js
@@ -405,6 +405,27 @@ function removeStyleElIfPresent() {
     top.document.getElementById(STYLE_EL_ID).remove()
   }
 }
+/**
+ * Sets the theme of the Cypress command log, using the environment settings.
+ * Defaults to `system`. Can be set to `light` or `dark` using the `THEME` configuration.
+ * (Alternatively, the `CYRPRESS_THEME` environment variable, from the command line).
+ * 
+ * @example
+ ```
+ // e2e.js or component.js
+ import setLightTheme from 'cypress-light-theme'
+ setLightTheme();
+
+ // Optionally:
+ // cypress.config.js
+ const { defineConfig } = require('cypress')
+ module.exports = defineConfig({
+     env: {
+         THEME: 'light' // or 'dark' or 'system'
+     }
+ });
+ ```
+ */
 export default function setLightTheme() {
   removeStyleElIfPresent()
   // get theme from env var
